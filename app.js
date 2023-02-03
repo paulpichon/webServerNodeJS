29bc1e6e0e6c2e5c21a3ac1e2228b397be8b5f3c
paul pichon <paul10_barca@hotmail.com>
2023-02-02 20:01:50 -0600

    Request y Response

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const http = require('http');
 
 http.createServer( (req, res) => {
 
-    res.write('Hola Mundo');
+    //console.log( req );
+    //res.writeHead( 200, { 'Content-Type': 'application/json'} );
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv' );
+    res.writeHead( 200, { 'Content-Type': 'application/csv'} );
+
+    res.write( 'id, nombre\n' );
+    res.write( '1, Magaly\n' );
+    res.write( '2, Julieta\n' );
+    res.write( '3, Ariadna\n' );
+    res.write( '4, Jessica\n' );
     //terminar respuesta
     res.end();
 
