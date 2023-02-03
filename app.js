e1fafd326754f0dc2274da30f757f4f4962276eb
paul pichon <paul10_barca@hotmail.com>
2023-02-02 21:13:07 -0600

    Servir contenido estatico, creacion de rutas, carpeta publica

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('HOME')
-});
+//middelware
+//servir contenido estatico
+app.use( express.static('public') );
 
+//
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola Mundo')
 });
 //*comodin, cualquier otra ruta que no sea las de arriba llegara aqui
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
+    //se puede poner un path para mostra alguna pagina
+    //sendFile
+    //__dirname para componer el path completo
+    res.sendFile( __dirname + '/public/404.html');
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
